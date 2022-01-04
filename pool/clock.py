d106893744ae5b8558ad034effc22bc412aff6ca
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-04 17:39:54 -0500

    Update clock.py

--- pool/clock.py
+++ pool/clock.py
@@ -5,8 +5,9 @@ sched = BlockingScheduler()
 
 
 
-@sched.scheduled_job(addMoneyToWinners(),'cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job()
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
+    addMoneyToWinners()
 
 sched.start()

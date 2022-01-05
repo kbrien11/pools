56c119905a50709e30facb9b45b36717eb3eeffc
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-05 10:53:09 -0500

    Update clock.py

--- pool/clock.py
+++ pool/clock.py
@@ -1,11 +1,17 @@
-
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from .views import addMoneyToWinners
 
+sched = BlockingScheduler()
+
+
+
+@sched.scheduled_job(addMoneyToWinners,day_of_week ='wed',hour=11)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
-    
-    addMoneyToWinners()
-   
+
+
+sched.start()
 
 

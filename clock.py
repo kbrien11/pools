3c2c9c4a82813ff401cf43ac1c7a41e30be1df78
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-06 16:52:22 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,22 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pools.settings'
+
+
+
+sched = BlockingScheduler()
 
 
 
 
 
+@sched.scheduled_job( 'interval', minutes=1)
 def scheduled_job():
-   print('This job is run every weekday at 5pm.')
+    print('This job is run every weekday at 5pm.')
+ 
+
+
+sched.start()
 

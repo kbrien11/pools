f30d2625087ffbdba913bb153880fb2a381c62a6
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-06 11:57:32 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,5 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pools.settings'
 
 
 

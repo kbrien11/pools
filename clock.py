f274e83101894e146619ba0b811683bd8fa6b36e
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-06 10:04:21 -0500

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+
+sched = BlockingScheduler()
+
+def do():
+    print("printing this scheduler')
+
+
+
+@sched.scheduled_job(do, 'interval', minutes=2)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+
+sched.start()

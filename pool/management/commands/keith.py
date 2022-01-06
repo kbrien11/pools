8b41c1ec1f9aca3e17bd47b240b626fdc798e4ff
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-06 12:33:14 -0500

    Add files via upload

---
+++ pool/management/commands/keith.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...views import addMoneyToWinners
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pools.settings'
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+
+
+    def handle(self, *args, **options):
+       print("running command")
+
+
+       sched = BlockingScheduler()
+
+       @sched.scheduled_job('interval', minutes=2)
+       def scheduled_job():
+           print('This job is run every weekday at 5pm.')
+           addMoneyToWinners()
+
+       sched.start()

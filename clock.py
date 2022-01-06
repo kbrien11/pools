9ad10fedf61e030627ee50fe89e143e9d6cff3ff
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-06 10:21:28 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,12 +5,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BlockingScheduler()
 
-def do():
-    print("printing this scheduler')
 
 
 
-@sched.scheduled_job(do, 'interval', minutes=2)
+
+@sched.scheduled_job( 'interval', minutes=2)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
 

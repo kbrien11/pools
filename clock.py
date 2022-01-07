585f39f664d11afce981542aeee0365aa03dba49
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-07 11:30:40 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,6 +2,19 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import json
+import re
+import pandas as pd
+import numpy as np
+import time,datetime, math
+from collections import defaultdict
+import os
+
+import environ
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pools.settings'
 
@@ -18,8 +31,6 @@ def scheduled_job():
     print('This job is run every weekday at 5pm.')
 
 
-    from pools.pool import views
-    views.addMoneyToWinners()
    
 
 sched.start()

6ab45b4f4adc8e09d38b77de0452315ba61d3755
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-07 16:38:18 -0500

    Update serializers.py

--- pool/serializers.py
+++ pool/serializers.py
@@ -39,7 +39,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
-        extra_kwargs = {'password': {'write_only': True, 'required': False},'first_name':{'required':False},'last_name':{'required':False},'email':{'required':False}}
+        extra_kwargs = {'password': {'required': True},'first_name':{'required':False},'last_name':{'required':False},'email':{'required':False}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)

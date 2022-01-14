c7e5aebe3fdaf81b094d039a95bac6ef01dd72af
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-14 13:44:04 -0500

    Update scrape.py

--- pool/scrape.py
+++ pool/scrape.py
@@ -7,6 +7,7 @@ import json
 import re
 import pandas as pd
 import numpy as np
+from .models import ScrapeData
 import time,datetime, math
 from collections import defaultdict
 import os
@@ -17,13 +18,12 @@ import environ
 CHROME_DRIVER = os.environ.get("CHROME_DRIVER")
 
 def open_link():
-    print("starting scaper")
     '''loads headless invisible browser on your machine and opens CBS NFL scores page.
         Returns: a google chrome browser'''
     opts = Options()
     opts.add_argument('--headless')
     # s = Service(r"C:\Users\kbrie\Downloads\chromedriver_win32/chromedriver")
-    web = webdriver.Chrome(options=opts,executable_path=CHROME_DRIVER) #can set executable path if needed here: executable_path='chromedriver'\
+    web = webdriver.Chrome(options=opts,executable_path=r"C:\Users\kbrie\Downloads\chromedriver_win32/chromedriver") #can set executable path if needed here: executable_path='chromedriver'\
     web.get('https://www.cbssports.com/nfl/scoreboard/')
     tot_scores = web.find_elements_by_xpath('.//td[@class="total-score"]')
     if len(tot_scores)==0:
@@ -33,7 +33,6 @@ def open_link():
 
 
 def pull_scores(web):
-    print("starting scaper")
     gms = web.find_elements_by_xpath('//div[contains(@id,"game")]')
     '''Use open_link function return value to populate correct web browser instance for score data'''
     num_names = {}
@@ -48,8 +47,10 @@ def pull_scores(web):
     num_names[9] = "5OT"
     num_names[10] = "6OT"
     gm_list = []
+    m = {}
     for idx,game in enumerate(gms):
         gm = defaultdict()
+        print(gm)
         gm['id'] = idx
         run_tot = 0
         run_tot2 = 0
@@ -61,43 +62,54 @@ def pull_scores(web):
             gm['total_away'] = totals[0].text
             gm['total_home'] = totals[1].text
         names = game.find_elements_by_xpath('.//a[contains(@class,"team")]')
-        gm['team_away'] = names[0].text
-        gm['team_home'] = names[1].text
+        if names[0].text in gm.values() or names[1].text in gm.values():
+            print("has already been scraped")
+            continue
+        else:
+            gm['team_away'] = names[0].text
+            gm['team_home'] = names[1].text
+            scraped_model = ScrapeData.objects.filter(home_team =names[1].text ,away_team = names[0].text).first()
+            if scraped_model is not None:
+                print("already been scapred")
+                pass
+            else:
+                new_scrape_model = ScrapeData(home_team =names[1].text ,away_team = names[0].text)
+                new_scrape_model.save()
+                scores = game.find_elements_by_xpath('.//td[@class="scores"]')
+                if len(scores)>0:
+                    for i, score in enumerate(scores):
+                        if score.text == '-':
+                            gm['status'] = 'in_progress'
+                            score_data = 0
+                        else:
+                            gm['status'] = 'completed'
+                            score_data = int(score.text)
 
-        scores = game.find_elements_by_xpath('.//td[@class="scores"]')
-        if len(scores)>0:
-            for i, score in enumerate(scores):
-                if score.text == '-':
-                    gm['status'] = 'in_progress'
-                    score_data = 0
-                else:
-                    gm['status'] = 'completed'
-                    score_data = int(score.text)
-                if len(scores)==8:
-                    if i <=3:
-                        key = num_names[i+1]
-                        gm["a"+str(i+1)] = score_data
-                        run_tot += int(score_data)
-                        gm[key] = [str(run_tot)[-1]]
-                    else:
-                        key = num_names[i-3]
-                        gm["h"+str(i-3)] = score_data
-                        run_tot2 += int(score_data)
-                        gm[key].append(str(run_tot2)[-1])
-                else:
-                    if i<=4:
-                        key = num_names[i+1]
-                        gm["a"+str(i+1)] = score_data
-                        run_tot += int(score_data)
-                        gm[key] = [str(run_tot)[-1]]
-                    else:
-                        key = num_names[i-4]
-                        gm["h"+str(i-4)] = score_data
-                        run_tot2 += int(score_data)
-                        gm[key].append(str(run_tot2)[-1])
+                        if len(scores)==8:
+                            if i <=3:
+                                key = num_names[i+1]
+                                gm["a"+str(i+1)] = score_data
+                                run_tot += int(score_data)
+                                gm[key] = [str(run_tot)[-1]]
+                            else:
+                                key = num_names[i-3]
+                                gm["h"+str(i-3)] = score_data
+                                run_tot2 += int(score_data)
+                                gm[key].append(str(run_tot2)[-1])
+                        else:
+                            if i<=4:
+                                key = num_names[i+1]
+                                gm["a"+str(i+1)] = score_data
+                                run_tot += int(score_data)
+                                gm[key] = [str(run_tot)[-1]]
+                            else:
+                                key = num_names[i-4]
+                                gm["h"+str(i-4)] = score_data
+                                run_tot2 += int(score_data)
+                                gm[key].append(str(run_tot2)[-1])
 
-            gm_list.append(gm)
-    print(gm_list)
+                    print(m)
+                    gm_list.append(gm)
     return gm_list
 
 

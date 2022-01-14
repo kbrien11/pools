eac1aaef30cb2d9095243ffdeac53c62d1211711
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2022-01-14 15:13:31 -0500

    Update models.py

--- pool/models.py
+++ pool/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.postgres.fields import ArrayField
 
 User._meta.get_field('username')._unique = True
 
